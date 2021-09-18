e69eddddfed85cf0d732365f6f5ae2b10f7a1cc9
firstrainbow2007 <firstrainbow2007@gmail.com>
2021-09-18 06:44:59 -0400

    first commit

---
+++ src/main/java/com/sample/gitsampleonenew/GitsampleonenewApplication.java
@@ -0,0 +1,13 @@
+package com.sample.gitsampleonenew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitsampleonenewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitsampleonenewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/gitsampleonenew/GitsampleonenewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.gitsampleonenew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitsampleonenewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
